b0bd348aedce159f955987e18b3ba9226fe44db6
Goga Octavian Gabriel <goga.o.gabriel.mac@gmail.com>
2018-04-29 11:17:47 +0300

    Implemented Despre

--- src/main/java/com/ase/ova/ro/ovaController.java
+++ src/main/java/com/ase/ova/ro/ovaController.java
@@ -1,17 +1,32 @@
 package com.ase.ova.ro;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.WindowEvent;
+import javafx.util.Pair;
 import javafx.util.StringConverter;
 
 /**
@@ -23,9 +38,15 @@ public class ovaController {
 
 	@FXML
 	private Button btnCautare;
-	
+
 	@FXML
 	private Button btnStergere;
+
+	@FXML
+	private MenuItem miDespre;
+
+	@FXML
+	private MenuItem miIesire;
 
 	@FXML
 	private CheckBox myCheckBox;
@@ -67,6 +88,54 @@ public class ovaController {
 		listViewData.add(new Person("Martin", "Mueller"));
 	}
 
+	void showAbout() {
+		// Create the custom dialog.
+		Dialog<Pair<String, String>> dialog = new Dialog<>();
+		dialog.setTitle("Realizatori");
+
+		Image image = new Image("/Realizatori1.png", 400, 500, false, false);
+		ImageView imageView = new ImageView(image);
+		imageView.fitWidthProperty();
+		imageView.fitHeightProperty();
+
+		// Set the icon (must be included in the project).
+
+		dialog.setResizable(false);
+		dialog.getDialogPane().setPrefSize(280, 290);
+
+		dialog.setGraphic(imageView);
+
+		// Set the button types.
+		ButtonType loginButtonType = new ButtonType("OK", ButtonData.OK_DONE);
+		dialog.getDialogPane().getButtonTypes().addAll(loginButtonType);
+
+		// Create the username and password labels and fields.
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		// grid.setPadding(new Insets(20, 150, 10, 10));
+
+		TextField username = new TextField();
+		username.setPromptText("Username");
+		PasswordField password = new PasswordField();
+		password.setPromptText("Password");
+
+		grid.add(new Label("Username:"), 0, 0);
+		grid.add(username, 1, 0);
+		grid.add(new Label("Password:"), 0, 1);
+		grid.add(password, 1, 1);
+
+		// Enable/Disable login button depending on whether a username was entered.
+		Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+		loginButton.setDisable(false);
+
+		// Do some validation (using the Java 8 lambda syntax).
+		username.textProperty().addListener((observable, oldValue, newValue) -> {
+			loginButton.setDisable(newValue.trim().isEmpty());
+		});
+		dialog.show();
+	}
+
 	/**
 	 * Initializes the controller class. This method is automatically called after
 	 * the fxml file has been loaded.
@@ -75,6 +144,7 @@ public class ovaController {
 	private void initialize() {
 		// Handle Button Cautare event.
 		btnCautare.setOnAction((event) -> {
+			// showDialog();
 			System.out.println("ButtonCautare action\n");
 			// outputTextArea.appendText("Button Action\n");
 		});
@@ -84,6 +154,18 @@ public class ovaController {
 			System.out.println("ButtonStergere action\n");
 			// outputTextArea.appendText("Button Action\n");
 		});
+
+		miDespre.setOnAction((event) -> {
+			showAbout();
+			System.out.println("ShowAbout action\n");
+		});
+
+		miIesire.setOnAction((event) -> {
+			Platform.exit();
+			System.exit(0);
+			System.out.println("Iesire action\n");
+		});
+
 		//
 		// // Handle CheckBox event.
 		// myCheckBox.setOnAction((event) -> {
