142b3985b094ecf61867c2b5359d2ce473a2b8b7
Goga Octavian Gabriel <goga.o.gabriel.mac@gmail.com>
2018-04-29 10:07:31 +0300

    implemented ovaController

--- src/main/java/com/ase/ova/ro/Main.java
+++ src/main/java/com/ase/ova/ro/Main.java
@@ -1,10 +1,19 @@
 package com.ase.ova.ro;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+
+
 
 /**
  * @Author : Gabriel Goga & Diana Ciuca & Alina Mirt & Alexandra Cocu
@@ -13,7 +22,8 @@ import javafx.stage.Stage;
  * @Version : 1.0
  */
 public class Main extends Application {
-	
+	@FXML
+    private ImageView imageView;
 	
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -25,8 +35,29 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(loader.<ScrollPane>getRoot(), 900, 700));
 
         primaryStage.show();
+        
+        
+      
     }
 
+	
+//	 @Override
+//	    public void start(Stage primaryStage) {
+//	        primaryStage.setTitle("Hello World!");
+//	        Button btn = new Button();
+//	        btn.setText("Say 'Hello World'");
+//	        btn.setOnAction(new EventHandler<ActionEvent>() {
+//	            public void handle(ActionEvent event) {
+//	                System.out.println("Hello World!");
+//	            }
+//	        });
+//	        
+//	        StackPane root = new StackPane();
+//	        root.getChildren().add(btn);
+//	        primaryStage.setScene(new Scene(root, 300, 250));
+//	        primaryStage.show();
+//	    }
+	
     public static void main(String[] args) {
         launch(args);
     }
